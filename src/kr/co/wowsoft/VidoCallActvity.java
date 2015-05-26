40a71330c5bb38461ef08854638744513807c472
wowsoft <help@wowsoft.co.kr>
2015-05-26 15:14:42 +0900

    메인화면 UI 개발

---
+++ src/kr/co/wowsoft/VidoCallActvity.java
@@ -0,0 +1,14 @@
+package kr.co.wowsoft;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class VidoCallActvity extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		 super.onCreate(savedInstanceState);
+		 setContentView(R.layout.activity_main);
+	}
+	
+}
